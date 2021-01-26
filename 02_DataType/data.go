eb5df3c0d005f1621562b00aa9c367a22ac02355
GoesToTwentyOne <nihadhossainthpibd373@gmail.com>
2021-01-26 21:05:51 +0600

    numbering error remove and upload DataType folder

---
+++ 02_DataType/data.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	var love bool = true //boolean type data
+	fmt.Println(love)
+	var x int = 10 // Integer type data
+	fmt.Println(x)
+	a := "My name is Nihad Hossain" //String type data
+	fmt.Println(a)
+}
