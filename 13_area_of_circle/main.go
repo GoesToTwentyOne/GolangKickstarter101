ce60851581c68d9407158d6fd0c57d2192ef0668
GoesToTwentyOne <nihadhossainthpibd373@gmail.com>
2021-01-27 20:58:09 +0600

    add defer use

--- 13_area_of_circle/main.go
+++ 13_area_of_circle/main.go
@@ -3,12 +3,11 @@ package main
 import "fmt"
 
 func main() {
-	pie := 3.14159
+	const pie = 3.14159
 	var R float64
 	fmt.Scan(&R)
 
 	A := pie * R * 2
-	fmt.Printf("A=%.4f", A)
-	fmt.Println()
+	fmt.Printf("A=%.4f \n", A)
 
 }
