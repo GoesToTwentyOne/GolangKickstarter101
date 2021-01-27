513bbc34dd45521666ec1321618fd299435f197f
GoesToTwentyOne <nihadhossainthpibd373@gmail.com>
2021-01-27 16:55:13 +0600

    add another file

--- 13_area_of_circle/main.go
+++ 13_area_of_circle/main.go
@@ -3,11 +3,11 @@ package main
 import "fmt"
 
 func main() {
-	π := 3.14159
+	pie := 3.14159
 	var R float64
 	fmt.Scan(&R)
 
-	A := π * R * 2
+	A := pie * R * 2
 	fmt.Printf("A=%.4f", A)
 	fmt.Println()
 
