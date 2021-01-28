285170c29126842ece7919049b32310a7eae9b56
GoesToTwentyOne <nihadhossainthpibd373@gmail.com>
2021-01-28 21:11:44 +0600

    number add project done

---
+++ 20_final_input_project_add_5num_sum/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+
+	input := bufio.NewReader(os.Stdin)
+	fmt.Println("Enter your several number you want add :")
+	check, _ := input.ReadString('\n')
+	checkconv, _ := strconv.ParseInt(strings.TrimSpace(check), 10, 64)
+
+	var sum int64
+	var i, j int64
+	for j = 1; j <= checkconv; j++ {
+		fmt.Printf("Enter your  add number %v:", j)
+		addnum, _ := input.ReadString('\n')
+		addnumconv, _ := strconv.ParseInt(strings.TrimSpace(addnum), 10, 64)
+		for i = j; i <= j; i++ {
+			sum = sum + addnumconv
+			//fmt.Println(sum)
+
+		}
+		//fmt.Println(sum)
+
+	}
+	fmt.Printf("add 5 value and result = %v \n", sum)
+
+}
