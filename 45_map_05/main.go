2ade71cb0fe6d160fb97ed5a41d0633b9463b742
GoesToTwentyOne <nihadhossainthpibd373@gmail.com>
2021-02-02 16:25:24 +0600

    add more map syntax

---
+++ 45_map_05/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	x := map[string]string{
+		"H": "Hello",
+		"R": "Room",
+		"F": "Fan",
+		"G": "Good",
+		"J": "Job",
+	}
+	fmt.Println(x["H"])
+
+}
