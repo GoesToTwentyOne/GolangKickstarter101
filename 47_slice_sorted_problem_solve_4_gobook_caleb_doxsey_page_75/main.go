d28a56010a141eeedbe041960933a323e46c9bb3
GoesToTwentyOne <nihadhossainthpibd373@gmail.com>
2021-02-07 23:40:48 +0600

    final change

--- 47_slice_sorted_problem_solve_4_gobook_caleb_doxsey_page_75/main.go
+++ 47_slice_sorted_problem_solve_4_gobook_caleb_doxsey_page_75/main.go
@@ -8,7 +8,7 @@ func main() {
 	var x = []int{
 		48, 96, 86, 68, 57, 82, 63, 70, 17, 34, 83, 27, 19, 97, 9, 17,
 	}
-	var sum int
+	var min int
 
 	var i int
 	var j int
@@ -16,16 +16,15 @@ func main() {
 	for i = 0; i < len(x); i++ {
 		for j = i + 1; j < len(x); j++ {
 			if x[i] < x[j] {
-				sum = x[i]
+				min = x[i]
 
 			}
-			//fmt.Println(sum)
 
 		}
-		//fmt.Println(sum)
+		//fmt.Println(min)
 
 	}
-	fmt.Println(sum)
+	fmt.Println(min)
 
 	fmt.Println()
 
