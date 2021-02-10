cc04c991930c2ed98c358ecae880db55034a9177
GoesToTwentyOne <nihadhossainthpibd373@gmail.com>
2021-02-10 19:42:35 +0600

    add defer function

---
+++ 74_defer_function_/main.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+	defer motherSon()
+	fatherSon()
+	sisterBrother()
+
+}
+func motherSon() {
+	fmt.Println("My son  my peace")
+
+}
+func fatherSon() {
+	fmt.Println("Little a boy ")
+
+}
+func sisterBrother() {
+	fmt.Println("He didn't give me choclate")
+
+}
