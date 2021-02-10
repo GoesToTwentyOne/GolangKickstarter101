3026e8cd683de16fc5f547214659677950a8ee57
GoesToTwentyOne <nihadhossainthpibd373@gmail.com>
2021-02-10 19:25:51 +0600

    add factorial function

--- 73_recursive_function_factorial/main.go
+++ 73_recursive_function_factorial/main.go
@@ -3,6 +3,7 @@ package main
 import "fmt"
 
 func main() {
+	//call function and pass value
 	fmt.Println(factorial(5))
 
 }
@@ -10,6 +11,7 @@ func factorial(n int) int {
 	if n == 0 {
 		return 1
 	}
+	//when n=0 it always enden
 	return n * factorial(n-1)
 
 }
