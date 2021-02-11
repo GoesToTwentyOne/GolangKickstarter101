cd2cd944fbeecc38ae1ea1549a2c96fbb07e0567
GoesToTwentyOne <nihadhossainthpibd373@gmail.com>
2021-02-11 17:45:53 +0600

    add_fibonacci_number_gobook_caleb_doxsey_page_91_problem_

---
+++ 80_fibonacci_number_gobook_caleb_doxsey_page_91_problem_5/main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(fib(10))
+
+}
+func fib(n int) int {
+	if n == 0 {
+		return 0
+	} else if n == 1 {
+		return 1
+	}
+	return fib(n-1) + fib(n-2)
+}
