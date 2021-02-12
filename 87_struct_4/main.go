abbf97e59e9afa73bdd0345d135208e4f218ee43
GoesToTwentyOne <nihadhossainthpibd373@gmail.com>
2021-02-12 20:40:03 +0600

    add circle struct

--- 87_struct_4/main.go
+++ 87_struct_4/main.go
@@ -1,9 +1,21 @@
 package main
 
+import (
+	"fmt"
+	"math"
+)
+
 func main() {
+	c := circle{0, 0, 5}
+	//fmt.Printf("%+v", c)
+	fmt.Println(circleArea(c))
 
 }
 
 type circle struct {
 	x, y, r float64
+}
+
+func circleArea(c circle) float64 {
+	return math.Pi * c.r * c.r
 }
